a34a0e4a080069e8836ac9a590365aaf39f5e843
marc <mreverson91@gmail.com>
2021-05-16 10:53:08 -0700

    first push - home and sections

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./styles/App.css";
+
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+function App() {
+  	return (
+	  	<div className="App">
+			  {/* <Header /> */}
+			  <Home />
+	  	</div>
+	);
+
+}
+
+export default App;
